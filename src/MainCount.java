e8f615c2f57509f66b59709d11fb6273daad0b14
mnowak1996 <35339817+mnowak1996@users.noreply.github.com>
2018-04-11 12:59:21 +0200

    layout

--- src/MainCount.java
+++ src/MainCount.java
@@ -7,11 +7,11 @@ import javafx.stage.Stage;
 public class MainCount extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainCount.class.getResource("UnitConverter.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainCount.class.getResource("CountWords.fxml"));
         Parent layout = fxmlLoader.load();
         Scene scene = new Scene(layout);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Konverter");
+        primaryStage.setTitle("Count words in a stirng");
         primaryStage.show();
     }
 }
