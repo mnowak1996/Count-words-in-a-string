62369d88feb3378c540768565c835ce5a8d47f83
mnowak1996 <35339817+mnowak1996@users.noreply.github.com>
2018-04-11 12:54:05 +0200

    begin

---
+++ src/MainCount.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainCount extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainCount.class.getResource("UnitConverter.fxml"));
+        Parent layout = fxmlLoader.load();
+        Scene scene = new Scene(layout);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Konverter");
+        primaryStage.show();
+    }
+}
